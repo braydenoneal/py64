bd0202ac31fab5ab6ed062c1d2b9a649e6a46f34
Brayden O'Neal <braydenioneal@gmail.com>
2026-03-17 10:59:23 -0500

    Implement support for second texture

--- assets/shaders/main/fragment.glsl
+++ assets/shaders/main/fragment.glsl
@@ -1,12 +1,23 @@
 #version 330 core
 
 uniform vec3 light;
-uniform sampler2D Texture;
+
 uniform vec3 solid_color;
-uniform bool use_texture;
-uniform ivec2 bounds;
-uniform vec2 texture_scale;
+
+uniform sampler2D texture_a;
+uniform bool use_texture_a;
+uniform ivec2 bounds_a;
+uniform vec2 texture_a_scale;
+
+uniform sampler2D texture_b;
+uniform bool use_texture_b;
+uniform ivec2 bounds_b;
+uniform vec2 texture_b_scale;
+
+uniform float texture_b_mix;
+
 uniform vec3 overlay_color;
+
 uniform float translucency;
 uniform int transparency_mode;
 
@@ -32,7 +43,7 @@ float wrap(float value, int bound) {
     return out_value;
 }
 
-vec2 wrap_uv(vec2 uv) {
+vec2 wrap_uv(vec2 uv, ivec2 bounds) {
     return vec2(wrap(uv.x, bounds.x), wrap(uv.y, bounds.y));
 }
 
@@ -48,7 +59,7 @@ vec4 clip_color(vec2 uv, ivec2 bounds, vec4 color) {
     return clip_color(uv.x, bounds.x, clip_color(uv.y, bounds.y, color));
 }
 
-vec4 filter_texture(sampler2D sampler, vec2 uv) {
+vec4 filter_texture(sampler2D sampler, vec2 uv, ivec2 bounds) {
     // From https://www.shadertoy.com/view/w3tcWM by Offline
     vec2 texture_size = vec2(textureSize(sampler, 0));
     vec2 pixel_size = 1.0 / texture_size;
@@ -57,9 +68,9 @@ vec4 filter_texture(sampler2D sampler, vec2 uv) {
     float side = step(0.0, pixel_position.x - pixel_position.y);
     vec2 offset = vec2(side, 1.0 - side) * pixel_size;
 
-    vec2 uv_a = wrap_uv(uv);
-    vec2 uv_b = wrap_uv(uv + pixel_size);
-    vec2 uv_c = wrap_uv(uv + offset);
+    vec2 uv_a = wrap_uv(uv, bounds);
+    vec2 uv_b = wrap_uv(uv + pixel_size, bounds);
+    vec2 uv_c = wrap_uv(uv + offset, bounds);
 
     vec4 color_a = clip_color(uv_a, bounds, texture(sampler, uv_a));
     vec4 color_b = clip_color(uv_b, bounds, texture(sampler, uv_b));
@@ -72,10 +83,14 @@ vec4 filter_texture(sampler2D sampler, vec2 uv) {
 }
 
 void main() {
-    if (use_texture) {
-        out_color = filter_texture(Texture, uv / texture_scale);
+    if (use_texture_a) {
+        out_color = filter_texture(texture_a, uv / texture_a_scale, bounds_a);
     } else {
         out_color = vec4(solid_color, 1);
+    }
+
+    if (use_texture_b) {
+        out_color = mix(out_color, filter_texture(texture_b, uv / texture_b_scale, bounds_b), texture_b_mix);
     }
 
     float luminance = dot(normalize(normal), normalize(light));
--- client/render/model/model.py
+++ client/render/model/model.py
@@ -1,4 +1,5 @@
 import json
+import os.path
 import struct
 from dataclasses import dataclass, astuple
 from typing import Any
@@ -83,25 +84,14 @@ class Material:
         self.program = program
         self.material_dict = material_dict
 
-        self.texture: Texture | None = None
+        self.texture_a: Texture | None = None
+        self.texture_b: Texture | None = None
 
         if 'texture' in self.material_dict.keys():
-            try:
-                image = Image.open(f'assets/textures/{self.material_dict["texture"]["name"]}.bmp').convert('RGBA').transpose(Transpose.FLIP_TOP_BOTTOM)
-                image_data = np.array(image, np.uint8).tobytes()
-                self.texture = self.ctx.texture(image.size, 4, image_data)
-                self.texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
-            except:
-                try:
-                    image = Image.open(f'assets/textures/{self.material_dict["texture"]["name"]}.png').convert('RGBA').transpose(Transpose.FLIP_TOP_BOTTOM)
-                    image_data = np.array(image, np.uint8).tobytes()
-                    self.texture = self.ctx.texture(image.size, 4, image_data)
-                    self.texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
-                except:
-                    image = Image.open(f'assets/textures/missing.png').convert('RGBA').transpose(Transpose.FLIP_TOP_BOTTOM)
-                    image_data = np.array(image, np.uint8).tobytes()
-                    self.texture = self.ctx.texture(image.size, 4, image_data)
-                    self.texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
+            self.texture_a = self.create_texture(self.material_dict['texture']['name'])
+
+        if 'second_texture' in self.material_dict.keys():
+            self.texture_b = self.create_texture(self.material_dict['second_texture']['name'])
 
         self.bytes = self.get_bytes()
         self.vbo = self.ctx.buffer(self.bytes)
@@ -109,6 +99,26 @@ class Material:
         self.vao = self.ctx.vertex_array(self.program, [
             (self.vbo, '3f 3f 2f 4f', 'in_vertex', 'in_normal', 'in_uv', 'in_color'),
         ])
+
+    def create_texture(self, name: str):
+        textures_root = 'assets/textures/'
+
+        path = f'{textures_root}missing.png'
+        bmp_path = f'{textures_root}{name}.bmp'
+        png_path = f'{textures_root}{name}.png'
+
+        if os.path.isfile(bmp_path):
+            path = bmp_path
+        elif os.path.isfile(png_path):
+            path = png_path
+
+        image = Image.open(path).convert('RGBA').transpose(Transpose.FLIP_TOP_BOTTOM)
+        image_data = np.array(image, np.uint8).tobytes()
+
+        texture = self.ctx.texture(image.size, 4, image_data)
+        texture.filter = (moderngl.NEAREST, moderngl.NEAREST)
+
+        return texture
 
     def get_bytes(self):
         bytes_data = b''
@@ -169,33 +179,58 @@ class Material:
         self.vbo.write(self.bytes)
 
     def render(self):
-        self.program['use_texture'] = False
         self.program['solid_color'] = vec3(0)
-        self.program['bounds'] = (0, 0)
-        self.program['texture_scale'] = (1, 1)
 
-        if self.texture:
-            self.texture.use()
+        self.program['use_texture_a'] = False
+        self.program['bounds_a'] = (0, 0)
+        self.program['texture_a_scale'] = (1, 1)
 
-            self.program['use_texture'] = True
+        self.program['use_texture_b'] = False
+        self.program['bounds_b'] = (0, 0)
+        self.program['texture_b_scale'] = (1, 1)
+
+        self.program['texture_b_mix'] = 0
+
+        if self.texture_a:
+            self.texture_a.use()
+
+            self.program['use_texture_a'] = True
 
             texture_dict = self.material_dict['texture']
+            bound_options = ('repeat', 'extend', 'clip', 'mirror')
 
-            self.program['bounds'] = (
-                ('repeat', 'extend', 'clip', 'mirror').index(texture_dict['bounds']['x']),
-                ('repeat', 'extend', 'clip', 'mirror').index(texture_dict['bounds']['y']),
+            self.program['bounds_a'] = (
+                bound_options.index(texture_dict['bounds']['x']),
+                bound_options.index(texture_dict['bounds']['y']),
             )
 
-            self.program['texture_scale'] = (
+            self.program['texture_a_scale'] = (
                 texture_dict['scale']['x'],
                 texture_dict['scale']['y'],
             )
         else:
-            self.program['solid_color'] = vec3(
-                self.material_dict['solid_color']['r'],
-                self.material_dict['solid_color']['g'],
-                self.material_dict['solid_color']['b'],
+            solid_color = self.material_dict['solid_color']
+            self.program['solid_color'] = vec3(solid_color['r'], solid_color['g'], solid_color['b'])
+
+        if self.texture_b:
+            self.texture_b.use(1)
+
+            self.program['use_texture_b'] = True
+
+            texture_dict = self.material_dict['second_texture']
+            bound_options = ('repeat', 'extend', 'clip', 'mirror')
+
+            self.program['bounds_b'] = (
+                bound_options.index(texture_dict['bounds']['x']),
+                bound_options.index(texture_dict['bounds']['y']),
             )
+
+            self.program['texture_b_scale'] = (
+                texture_dict['scale']['x'],
+                texture_dict['scale']['y'],
+            )
+
+            self.program['texture_b_mix'] = texture_dict['mix']
 
         if self.material_dict['backface_culling']:
             self.ctx.enable(moderngl.CULL_FACE)
